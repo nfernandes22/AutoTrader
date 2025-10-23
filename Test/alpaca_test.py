876ce2e970a57f5efc188395f1c187e76a29bdc1
Nicole Fernandes <nicole.fernandes-2@student.manchester.ac.uk>
2025-10-23 19:39:42 +0100

    Test/alpaca_test.py

--- Test/alpaca_test.py
+++ Test/alpaca_test.py
@@ -1,2 +1,12 @@
-import alpaca-py as alpaca
+from alpaca_trade_api import REST, TimeFrame
+import os
+
+API_KEY = os.getenv("APCA_API_KEY_ID")
+API_SECRET = os.getenv("APCA_API_SECRET_KEY")
+BASE_URL = "https://paper-api.alpaca.markets"
+
+api = REST(API_KEY, API_SECRET, BASE_URL)
+
+account = api.get_account()
+print(account.status)
 
